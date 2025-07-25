6a44e2602e40cd9daf4f44a298fb83b348e2d241
Geoffrey White <geoffpiercewhite@gmail.com>
2025-07-25 13:54:27 -0400

    wow this works nice

--- game/game.go
+++ game/game.go
@@ -17,22 +17,85 @@ const (
 
 type Game struct {
 	AP    *ansipixels.AnsiPixels
-	state gameState
+	State gameState
 }
 
 type gameState [4][4]int
 
+func NewGame(ap *ansipixels.AnsiPixels) *Game {
+	g := &Game{
+		AP:    ap,
+		State: gameState{},
+	}
+	g.AddOneInRandomSpot()
+	return g
+}
+
 func (g *Game) AddOneInRandomSpot() {
 	for {
 		x, y := rand.IntN(4), rand.IntN(4)
-		if g.state[x][y] != 0 {
+		if g.State[x][y] != 0 {
 			continue
 		}
-		g.state[x][y] = 1
+		g.State[x][y] = 1
 		return
 	}
 }
 
 func (g *Game) Left() {
+	//x--
+	for y := 0; y < 4; y++ {
+		for x := 1; x < 4; x++ {
+			var x2 int
+			for x2 = x; x2 != 0 && g.State[x2][y] != 0 && g.State[x2-1][y] == 0; x2-- {
+				g.State[x2][y], g.State[x2-1][y] = g.State[x2-1][y], g.State[x2][y]
+			}
+			if x2 != 0 && g.State[x2][y] == g.State[x2-1][y] {
+				g.State[x2][y], g.State[x2-1][y] = 0, g.State[x2-1][y]*2
+			}
+		}
+	}
+}
+
+func (g *Game) Right() {
+	for y := 0; y < 4; y++ {
+		for x := 2; x > -1; x-- {
+			var x2 int
+			for x2 = x; x2 != 3 && g.State[x2][y] != 0 && g.State[x2+1][y] == 0; x2++ {
+				g.State[x2][y], g.State[x2+1][y] = g.State[x2+1][y], g.State[x2][y]
+			}
+			if x2 != 3 && g.State[x2][y] == g.State[x2+1][y] {
+				g.State[x2][y], g.State[x2+1][y] = 0, g.State[x2+1][y]*2
+			}
+		}
+	}
+}
+func (g *Game) Up() {
+	for x := 0; x < 4; x++ {
+		for y := 1; y < 4; y++ {
+			var y2 int
+			for y2 = y; y2 != 0 && g.State[x][y2] != 0 && g.State[x][y2-1] == 0; y2-- {
+				g.State[x][y2], g.State[x][y2-1] = g.State[x][y2-1], g.State[x][y2]
+			}
+			if y2 != 0 && g.State[x][y2] == g.State[x][y2-1] {
+				g.State[x][y2], g.State[x][y2-1] = 0, g.State[x][y2-1]*2
+			}
+		}
+	}
+
+}
+func (g *Game) Down() {
+
+	for x := 0; x < 4; x++ {
+		for y := 2; y > -1; y-- {
+			var y2 int
+			for y2 = y; y2 != 3 && g.State[x][y2] != 0 && g.State[x][y2+1] == 0; y2++ {
+				g.State[x][y2], g.State[x][y2+1] = g.State[x][y2+1], g.State[x][y2]
+			}
+			if y2 != 3 && g.State[x][y2] == g.State[x][y2+1] {
+				g.State[x][y2], g.State[x][y2+1] = 0, g.State[x][y2+1]*2
+			}
+		}
+	}
 
 }
--- main.go
+++ main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"fortio/2048/game"
 
 	"fortio.org/log"
 	"fortio.org/terminal/ansipixels"
 )
+
+const (
+	ONE                   = ansipixels.Red
+	TWO                   = ansipixels.Green
+	FOUR                  = ansipixels.Yellow
+	EIGHT                 = ansipixels.Blue
+	SIXTEEN               = ansipixels.Purple
+	THIRTYTWO             = ansipixels.Cyan
+	SIXTYFOUR             = ansipixels.Gray
+	ONEHUNDREDTWENTYEIGHT = ansipixels.DarkGray
+	TWOHUNDREDFIFTYSIX    = ansipixels.BrightGreen
+)
+
+var numColors = map[int]string{
+	1:   ONE,
+	2:   TWO,
+	4:   FOUR,
+	8:   EIGHT,
+	16:  SIXTEEN,
+	32:  THIRTYTWO,
+	64:  SIXTYFOUR,
+	128: ONEHUNDREDTWENTYEIGHT,
+	256: TWOHUNDREDFIFTYSIX,
+}
 
 func main() {
 	ap := ansipixels.NewAnsiPixels(0)
@@ -25,14 +49,43 @@ func main() {
 	}
 
 	defer ap.Restore()
-	for i := 0; i < ap.W-4; i += ap.W / 4 {
-		for j := 0; j < ap.H-4; j += ap.H / 4 {
-			ap.ClearScreen()
-			ap.StartSyncMode()
-			ap.DrawSquareBox(i, j, ap.W/4, ap.H/4)
-			ap.WriteAtStr(i+ap.W/8, j+ap.H/8, fmt.Sprintf("%d %d", i, j))
-			time.Sleep(200 * (time.Millisecond))
-			ap.EndSyncMode()
+	defer ap.ClearScreen()
+	ap.ClearScreen()
+	game := game.NewGame(ap)
+	for {
+		wi, hi := 0, 0
+
+		for i := 0; i < ap.W-4; i += ap.W / 4 {
+			hi = 0
+			for j := 0; j < ap.H-4; j += ap.H / 4 {
+				ap.StartSyncMode()
+				ap.DrawColoredBox(i, j, ap.W/4, ap.H/4, numColors[game.State[wi][hi]], false)
+				ap.WriteAtStr(i+ap.W/8, j+ap.H/8, fmt.Sprintf("%d", game.State[wi][hi]))
+				// time.Sleep(200 * (time.Millisecond))
+				ap.EndSyncMode()
+				hi++
+			}
+			wi++
+		}
+		_, err = ap.ReadOrResizeOrSignalOnce()
+		if err != nil {
+			log.FErrf("Error reading: %v", err)
+		}
+		switch ap.Data[0] {
+		case 37, 'a': // left
+			game.Left()
+			game.AddOneInRandomSpot()
+		case 38, 'w': // up
+			game.Up()
+			game.AddOneInRandomSpot()
+		case 39, 'd': // right
+			game.Right()
+			game.AddOneInRandomSpot()
+		case 40, 's': // down
+			game.Down()
+			game.AddOneInRandomSpot()
+		case 'q':
+			return
 		}
 	}
 
